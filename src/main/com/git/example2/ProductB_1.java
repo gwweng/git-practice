6b6d2d8f011596c5b6cc19ab9aaa5c2cd764f917
翁 <gengweiweng@gmail.com>
2023-04-24 22:54:08 +0800

    功能B开发分支（包含B_1,B_2子需求）

---
+++ src/main/com/git/example2/ProductB_1.java
@@ -0,0 +1,7 @@
+package com.git.example2;
+
+/**
+ * 张三开发功能B_1需求
+ */
+public class ProductB_1 {
+}
---
+++ src/main/com/git/example2/ProductB_2.java
@@ -0,0 +1,7 @@
+package com.git.example2;
+
+/**
+ * 张三开发功能B_2
+ */
+public class ProductB_2 {
+}
